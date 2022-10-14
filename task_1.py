d86fb16f7bae92ec79b253a24beeb3ee25754698
Kacper <kacper.wroblewski651@gmail.com>
2022-10-15 00:01:45 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,13 @@
-def cross(n):
-    pass
+#CROSS
+
+def cross(n):                               #rysowanie krzyża z "kwadratów "o boku 4
+    for i in range(n):
+        print(n * " " + n * "*" + n * " ")
+    for i in range(n):
+        print(3 * n * "*")
+    for i in range(n):
+        print(n * " " + n * "*" + n * " ")
+
+
+cross(4)
+
