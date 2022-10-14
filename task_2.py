0bd4b8e5b14c32f82c5427c457efe9adb848a22d
Kacper <kacper.wroblewski651@gmail.com>
2022-10-15 00:28:47 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,13 @@
+# CHESS BOARD
+# pewnie da się lepiej przykminić albo rozdzielić te funkcje ale idk
 def chess_board(n, k):
-    pass
+    for i in range(2 * n):
+        if i % 2 == 0:
+            print(n * (2 * "  " + 2 * "##"))
+            print(n * (2 * "  " + 2 * "##"))
+        else:
+            print(n * (2 * "##" + 2 * "  "))
+            print(n * (2 * "##" + 2 * "  "))
+
+
+chess_board(4, 2)
