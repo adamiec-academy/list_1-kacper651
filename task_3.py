5a68a5052489b20295bf263d0adfa67d05162047
kacper651 <kacper.wroblewski651@gmail.com>
2022-10-23 14:29:24 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -6,12 +6,12 @@ def envelope(n):
     print((2 * n + 1) * "*")  # PODSTAWA GÓRNA
 
     for i in range(n - 1):
-        print("*" + i * " " + "*" + (math.ceil((2 * n + 1) / 2) - 2 * i) * " " + "*" + i * " " + "*")  # CIAŁO 1
+        print("*" + i * " " + "*" + (2 * (n - i) - 3) * " " + "*" + i * " " + "*")  # CIAŁO 1, nwm jak ale chyba działa hihi
 
-    print("*" + math.floor((2 * n + 1) / 3) * " " + "*" + math.floor((2 * n + 1) / 3) * " " + "*")  # ŚRODEK
+    print("*" + (n - 1) * " " + "*" + (n - 1) * " " + "*") # ŚRODEK
 
     for i in range(n - 2, 0, -1):
-        print("*" + i * " " + "*" + (math.ceil((2 * n + 1) / 2) - 2 * i) * " " + "*" + i * " " + "*")  # CIAŁO 2
+        print("*" + i * " " + "*" + (2 * (n - i) - 3) * " " + "*" + i * " " + "*")
 
     print((2 * n + 1) * "*")  # PODSTAWA DOLNA
 
