f3b27ed9ed748230728e42b8e681a6da55d689d3
Kacper <kacper.wroblewski651@gmail.com>
2022-10-24 01:06:58 +0200

    finished tasks

--- task_5.py
+++ task_5.py
@@ -1,6 +1,13 @@
-def factorial(n):
-    pass
+def factorial(n):  # chyba najprostsze zadanie, ale może dlatego, że pisałem silnie 100 razy, nawet w asemblerze xd
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 
 def report():
-    pass
+    for i in range(101):
+        print(f"{i}! is {len(str(factorial(i)))} digits long")  # tego fstringa podpatrzyłem z zajęć sorry
+
+
+report()
