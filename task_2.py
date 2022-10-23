5bca0b35fa46b1d3da1b68e4b908fad2b8c86665
kacper651 <kacper.wroblewski651@gmail.com>
2022-10-23 13:46:14 +0200

    task_2 updated

--- task_2.py
+++ task_2.py
@@ -1,11 +1,10 @@
 # CHESS BOARD
-# pewnie da się lepiej przykminić albo rozdzielić te funkcje ale idk
 def chess_board(n, k):
-    for i in range(n):
-        print(n * (k * " " + k * "#"))
-        print(n * (k * " " + k * "#"))
-        print(n * (k * "#" + k * " "))
-        print(n * (k * "#" + k * " "))
+    for _ in range(n):
+        for _ in range(k):
+            print(n * (k * " " + k * "#"))
+        for _ in range(k):
+            print(n * (k * "#" + k * " "))
 
 
 chess_board(4, 2)
